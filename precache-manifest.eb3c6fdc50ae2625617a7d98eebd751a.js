b8199c9842d6486834c6d4e59c3bebe16b3b2dae
any-p <54002397+any-p@users.noreply.github.com>
2019-08-14 15:52:04 +0300

    Updates

---
+++ precache-manifest.eb3c6fdc50ae2625617a7d98eebd751a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f42c2dae52f36f3a8cd5f65e0435f61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89d4def6ff6edb0e686d",
+    "url": "/static/css/main.20e6598f.chunk.css"
+  },
+  {
+    "revision": "dac23aba5e3259e4be6f",
+    "url": "/static/js/2.7af02deb.chunk.js"
+  },
+  {
+    "revision": "89d4def6ff6edb0e686d",
+    "url": "/static/js/main.4607373b.chunk.js"
+  },
+  {
+    "revision": "51e2619540d48775a63a",
+    "url": "/static/js/runtime~main.ea96ba6a.js"
+  }
+]);
