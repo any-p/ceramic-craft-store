b12abafeeb441aee481b3112456d1c1ca826da69
any-p <54002397+any-p@users.noreply.github.com>
2019-08-14 15:03:20 +0300

    1 add company name

--- src/Header/index.jsx
+++ src/Header/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 // import logo from './logo.svg';
-// import './App.css';
+import './Header.css';
 
 function Header() {
   return (
     <header className="App-header">
     {/* <img src={logo} className="App-logo" alt="logo" /> */}
+    <h1 className="company-name">ORGANICA</h1>
     <a
         className="App-link"
         href="tel: +7 981 956-81-25"
