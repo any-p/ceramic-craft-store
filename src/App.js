05c8e9cdb496c5284f86c4163c5b96ca7b05fa04
any-p <54002397+any-p@users.noreply.github.com>
2019-08-15 15:48:01 +0300

    3 Create InfoSection

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import Header from "./Header"
+import Header from './Header';
+import InfoSection from './InfoSection';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <InfoSection/>
     </div>
   );
 }
---
+++ src/InfoSection/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function InfoSection () {
+    return (
+        <section>
+            <h1>Мастер классы</h1>
+        </section>
+    )
+}
+
+export default InfoSection;
