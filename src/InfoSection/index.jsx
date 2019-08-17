58eb72cec05e15bfc16fd4c8b3e076ff8ad75181
pure-js <3690368+pure-js@users.noreply.github.com>
2019-08-17 20:51:02 +0300

    7 eslint fixes

--- src/InfoSection/index.jsx
+++ src/InfoSection/index.jsx
@@ -5,7 +5,11 @@ import './InfoSection.css';
 const InfoSection = () => (
   <>
     <section className="dark-section">
-      <h1 className="dark-section__text">Просто лучшая студия <br/> Керамики в городе!</h1>
+      <h1 className="dark-section__text">
+        Просто лучшая студия
+        <br />
+        Керамики в городе!
+      </h1>
       <h2 className="dark-section__text m-t-30">Но это не точно ...</h2>
       <div className="centralize">
         <button type="button" className="light-btn_primary m-t-50">
@@ -15,7 +19,7 @@ const InfoSection = () => (
     </section>
     <section className="info-section">
       <Link to="/master-class/" className="info-section__text">
-        <h1 >Узнать подробнее о мастер-классах</h1>
+        <h1>Узнать подробнее о мастер-классах</h1>
       </Link>
     </section>
   </>
