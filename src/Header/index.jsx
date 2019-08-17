8f39dd9fbcc721bcf0cf3a31200041661ee3df93
pure-js <3690368+pure-js@users.noreply.github.com>
2019-08-17 13:48:12 +0300

    4 add ESLint

--- src/Header/index.jsx
+++ src/Header/index.jsx
@@ -5,16 +5,16 @@ import './Header.css';
 function Header() {
   return (
     <header className="App-header">
-    {/* <img src={logo} className="App-logo" alt="logo" /> */}
-    <h1 className="company-name">ORGANICA</h1>
-    <a
+      {/* <img src={logo} className="App-logo" alt="logo" /> */}
+      <h1 className="company-name">ORGANICA</h1>
+      <a
         className="App-link"
         href="tel: +7 981 956-81-25"
         target="_blank"
         rel="noopener noreferrer"
-    >
+      >
         +7 981 956-81-25
-    </a>
+      </a>
     </header>
   );
 }
